b3e2a60cf6eb0d270ea3c516b755666371d2c7d8
tonyd <tonyd@ANTHONY-PC>
2017-01-10 00:18:38 -0600

    Tony 17

--- src/java/Manejador/ManejadorPagos.java
+++ src/java/Manejador/ManejadorPagos.java
@@ -4,11 +4,16 @@ import Datos.DatoPago;
 import Modelo.Ingreso;
 import Modelo.Peaje;
 import java.io.Serializable;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
 import javax.faces.bean.SessionScoped;
+import org.primefaces.context.RequestContext;
+import org.primefaces.event.SelectEvent;
    
 @ManagedBean
 @ViewScoped
@@ -21,7 +26,29 @@ public class ManejadorPagos implements Serializable {
     private List<Ingreso> lstIngreso;
     private String accion;
     private String fechaFinal;
+    private Date date10;
+     
+    public void onDateSelect(SelectEvent event) {
+        FacesContext facesContext = FacesContext.getCurrentInstance();
+        SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+        facesContext.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Date Selected", format.format(event.getObject())));
+    }
+     
+    public void click() {
+        RequestContext requestContext = RequestContext.getCurrentInstance();
+         
+        requestContext.update("form:display");
+        requestContext.execute("PF('dlg').show()");
+    }
 
+    public Date getDate10() {
+        return date10;
+    }
+
+    public void setDate10(Date date10) {
+        this.date10 = date10;
+    }
+    
     public Peaje getPeaje() {
         return peaje;
     }
